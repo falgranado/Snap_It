ead59c5a032b5a4c402afcce0db1cdb049ab7d39
Francisco A. Granado Santos <francisco.granado1@upr.edu>
2014-06-07 14:56:37 -0400

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/**
+ * @author Ch32k0
+ */
+
+function initialize(){
+	initialize_parse();
+}
+
+function initialize_parse(){
+	Parse.initialize("VHIOD6mvaPCGFl4rA0guHyOZlmOg3knU5y1RfKmG","BzYeBsAlR40wIQu70iZIaCWnQI7CYVUpy2inO2nJ");
+}
+$(document).ready(function(){
+	
+});
